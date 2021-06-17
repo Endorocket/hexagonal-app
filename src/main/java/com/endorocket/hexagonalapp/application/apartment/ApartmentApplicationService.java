c04ff17ce9ee83608a7c8ea78dcd3bd387f110ab
Endorocket <endorocket@gmail.com>
2021-06-18 00:53:39 +0200

    Adding new Apartment - move creation of Apartment from ApartmentApplicationService to ApartmentFactory

--- src/main/java/com/endorocket/hexagonalapp/application/apartment/ApartmentApplicationService.java
+++ src/main/java/com/endorocket/hexagonalapp/application/apartment/ApartmentApplicationService.java
@@ -1,28 +1,15 @@
 package com.endorocket.hexagonalapp.application.apartment;
 
-import com.endorocket.hexagonalapp.domain.apartment.Address;
 import com.endorocket.hexagonalapp.domain.apartment.Apartment;
-import com.endorocket.hexagonalapp.domain.apartment.Room;
-import com.endorocket.hexagonalapp.domain.apartment.SquareMeter;
+import com.endorocket.hexagonalapp.domain.apartment.ApartmentFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class ApartmentApplicationService {
 	public void add(String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber,
 	                String city, String country, String description, Map<String, Double> roomsDefinition) {
 
-		Address address = new Address(street, postalCode, houseNumber, apartmentNumber, city, country);
-		List<Room> rooms = roomsDefinition.entrySet().stream()
-			.map(entry -> {
-				String name = entry.getKey();
-				Double size = entry.getValue();
-				SquareMeter squareMeter = new SquareMeter(size);
-				return new Room(name, squareMeter);
-			})
-			.toList();
-		Apartment apartment = new Apartment(ownerId, address, description);
+		Apartment apartment = new ApartmentFactory().create(ownerId, street, postalCode, houseNumber, apartmentNumber, city, country, description, roomsDefinition);
 
 	}
 }
---
+++ src/main/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentFactory.java
@@ -0,0 +1,20 @@
+package com.endorocket.hexagonalapp.domain.apartment;
+
+import java.util.List;
+import java.util.Map;
+
+public class ApartmentFactory {
+	public Apartment create(String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber,
+	                        String city, String country, String description, Map<String, Double> roomsDefinition) {
+		Address address = new Address(street, postalCode, houseNumber, apartmentNumber, city, country);
+		List<Room> rooms = roomsDefinition.entrySet().stream()
+			.map(entry -> {
+				String name = entry.getKey();
+				Double size = entry.getValue();
+				SquareMeter squareMeter = new SquareMeter(size);
+				return new Room(name, squareMeter);
+			})
+			.toList();
+		return new Apartment(ownerId, address, description);
+	}
+}
