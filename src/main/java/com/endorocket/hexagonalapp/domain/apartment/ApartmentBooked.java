2b0756f7797c691d037c2f2232e68987bcc0cf9d
Endorocket <endorocket@gmail.com>
2021-06-19 19:04:20 +0200

    Book an Apartment - add getters to ApartmentBooked and use LocalDates instead of Period

--- src/main/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentBooked.java
+++ src/main/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentBooked.java
@@ -1,24 +1,26 @@
 package com.endorocket.hexagonalapp.domain.apartment;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-class ApartmentBooked {
+public class ApartmentBooked {
 	private final String eventId;
 	private final LocalDateTime creationDateTime;
 	private final String id;
 	private final String ownerId;
 	private final String tenantId;
-	private final Period period;
+	private final LocalDate periodStart;
+	private final LocalDate periodEnd;
 
 	private ApartmentBooked(String eventId, LocalDateTime creationDateTime, String id, String ownerId, String tenantId, Period period) {
-
 		this.eventId = eventId;
 		this.creationDateTime = creationDateTime;
 		this.id = id;
 		this.ownerId = ownerId;
 		this.tenantId = tenantId;
-		this.period = period;
+		periodStart = period.getStart();
+		periodEnd = period.getEnd();
 	}
 
 	static ApartmentBooked create(String id, String ownerId, String tenantId, Period period) {
@@ -26,5 +28,33 @@ class ApartmentBooked {
 		LocalDateTime creationDateTime = LocalDateTime.now();
 
 		return new ApartmentBooked(eventId, creationDateTime, id, ownerId, tenantId, period);
+	}
+
+	public String getEventId() {
+		return eventId;
+	}
+
+	public LocalDateTime getCreationDateTime() {
+		return creationDateTime;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getOwnerId() {
+		return ownerId;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public LocalDate getPeriodStart() {
+		return periodStart;
+	}
+
+	public LocalDate getPeriodEnd() {
+		return periodEnd;
 	}
 }
