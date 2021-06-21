ee7e713d151878a45b02737d87e169f5d7fbb6d9
Endorocket <endorocket@gmail.com>
2021-06-21 21:49:07 +0200

    Apartment - book - move apartment input data to constants

--- src/test/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentTest.java
+++ src/test/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentTest.java
@@ -9,24 +9,24 @@ import java.util.function.Consumer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApartmentTest {
+	private static final String OWNER_ID = "1234";
+	private static final String STREET = "Krakowska";
+	private static final String POSTAL_CODE = "12-345";
+	private static final String HOUSE_NUMBER = "1";
+	private static final String APARTMENT_NUMBER = "12";
+	private static final String CITY = "Wrocław";
+	private static final String COUNTRY = "Poland";
+	private static final String DESCRIPTION = "Nice apartment";
+	private static final Map<String, Double> ROOMS_DEFINITION = Map.of("Toilet", 10.0, "Bedroom", 15.5);
+
 	@Test
 	void shouldCreateApartmentWithAllRequiredFields() {
-		String ownerId = "1234";
-		String street = "Krakowska";
-		String postalCode = "12-345";
-		String houseNumber = "1";
-		String apartmentNumber = "12";
-		String city = "Wrocław";
-		String country = "Poland";
-		String description = "Nice apartment";
-		Map<String, Double> roomsDefinition = Map.of("Toilet", 10.0, "Bedroom", 15.5);
+		Apartment actual = new ApartmentFactory().create(OWNER_ID, STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY, DESCRIPTION, ROOMS_DEFINITION);
 
-		Apartment actual = new ApartmentFactory().create(ownerId, street, postalCode, houseNumber, apartmentNumber, city, country, description, roomsDefinition);
-
-		assertThatHasOwnerId(actual, ownerId);
-		assertThatHasDescription(actual, description);
-		assertThatHasAddress(actual, street, postalCode, houseNumber, apartmentNumber, city, country);
-		assertThatHasRooms(actual, roomsDefinition);
+		assertThatHasOwnerId(actual, OWNER_ID);
+		assertThatHasDescription(actual, DESCRIPTION);
+		assertThatHasAddress(actual, STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY);
+		assertThatHasRooms(actual, ROOMS_DEFINITION);
 	}
 
 	private void assertThatHasOwnerId(Apartment actual, String ownerId) {
