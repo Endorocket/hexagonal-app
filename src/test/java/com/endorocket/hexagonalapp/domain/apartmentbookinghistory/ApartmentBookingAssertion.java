ab2e64f43ccae581e54a95dda16e62ed6b7071cf
Endorocket <endorocket@gmail.com>
2021-06-21 17:21:11 +0200

    Unit Tests - fluent assertions of ApartmentBookingTest

---
+++ src/test/java/com/endorocket/hexagonalapp/domain/apartmentbookinghistory/ApartmentBookingAssertion.java
@@ -0,0 +1,45 @@
+package com.endorocket.hexagonalapp.domain.apartmentbookinghistory;
+
+import org.assertj.core.api.Assertions;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ApartmentBookingAssertion {
+	private final ApartmentBooking actual;
+
+	private ApartmentBookingAssertion(ApartmentBooking actual) {
+		this.actual = actual;
+	}
+
+	static ApartmentBookingAssertion assertThat(ApartmentBooking actual) {
+		return new ApartmentBookingAssertion(actual);
+	}
+
+	ApartmentBookingAssertion isStart() {
+		Assertions.assertThat(actual).hasFieldOrPropertyWithValue("bookingStep", BookingStep.START);
+		return this;
+	}
+
+	ApartmentBookingAssertion hasBookingDateTimeEqualTo(LocalDateTime expected) {
+		Assertions.assertThat(actual).hasFieldOrPropertyWithValue("bookingDateTime", expected);
+		return this;
+	}
+
+	ApartmentBookingAssertion hasOwnerIdEqualTo(String expected) {
+		Assertions.assertThat(actual).hasFieldOrPropertyWithValue("ownerId", expected);
+		return this;
+	}
+
+	ApartmentBookingAssertion hasTenantIdEqualTo(String expected) {
+		Assertions.assertThat(actual).hasFieldOrPropertyWithValue("tenantId", expected);
+		return this;
+	}
+
+	ApartmentBookingAssertion hasBookingPeriodThatHas(LocalDate expectedStart, LocalDate expectedEnd) {
+		Assertions.assertThat(actual)
+			.hasFieldOrPropertyWithValue("bookingPeriod.periodStart", expectedStart)
+			.hasFieldOrPropertyWithValue("bookingPeriod.periodEnd", expectedEnd);
+		return this;
+	}
+}
---
+++ src/test/java/com/endorocket/hexagonalapp/domain/apartmentbookinghistory/ApartmentBookingTest.java
@@ -0,0 +1,29 @@
+package com.endorocket.hexagonalapp.domain.apartmentbookinghistory;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.endorocket.hexagonalapp.domain.apartmentbookinghistory.ApartmentBookingAssertion.assertThat;
+
+class ApartmentBookingTest {
+
+	@Test
+	void shouldCreateStartApartmentBookingWithAllRequiredInformation() {
+		LocalDateTime bookingDateTime = LocalDateTime.of(2020, 1, 2, 3, 4);
+		String ownerId = "1234";
+		String tenantId = "5678";
+		LocalDate start = LocalDate.of(2020, 2, 1);
+		LocalDate end = LocalDate.of(2020, 2, 8);
+
+		ApartmentBooking actual = ApartmentBooking.start(bookingDateTime, ownerId, tenantId, new BookingPeriod(start, end));
+
+		assertThat(actual)
+			.isStart()
+			.hasBookingDateTimeEqualTo(bookingDateTime)
+			.hasOwnerIdEqualTo(ownerId)
+			.hasTenantIdEqualTo(tenantId)
+			.hasBookingPeriodThatHas(start, end);
+	}
+}
