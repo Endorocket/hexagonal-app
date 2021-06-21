7c71b9650b376f3c6ed72a83a4065ae288c47c78
Endorocket <endorocket@gmail.com>
2021-06-21 12:39:21 +0200

    Unit Tests - add implementation of assertions

--- src/test/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentTest.java
+++ src/test/java/com/endorocket/hexagonalapp/domain/apartment/ApartmentTest.java
@@ -2,7 +2,10 @@ package com.endorocket.hexagonalapp.domain.apartment;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ApartmentTest {
 	@Test
@@ -26,18 +29,36 @@ class ApartmentTest {
 	}
 
 	private void assertThatHasOwnerId(Apartment actual, String ownerId) {
-
+		assertThat(actual).hasFieldOrPropertyWithValue("ownerId", ownerId);
 	}
 
 	private void assertThatHasDescription(Apartment actual, String description) {
-
+		assertThat(actual).hasFieldOrPropertyWithValue("description", description);
 	}
 
 	private void assertThatHasAddress(Apartment actual, String street, String postalCode, String houseNumber, String apartmentNumber, String city, String country) {
-
+		assertThat(actual).extracting("address")
+			.hasFieldOrPropertyWithValue("street", street)
+			.hasFieldOrPropertyWithValue("postalCode", postalCode)
+			.hasFieldOrPropertyWithValue("houseNumber", houseNumber)
+			.hasFieldOrPropertyWithValue("apartmentNumber", apartmentNumber)
+			.hasFieldOrPropertyWithValue("city", city)
+			.hasFieldOrPropertyWithValue("country", country);
 	}
 
+	@SuppressWarnings("unchecked")
 	private void assertThatHasRooms(Apartment actual, Map<String, Double> roomsDefinition) {
+		assertThat(actual).extracting("rooms").satisfies(roomsActual -> {
+			List<Room> rooms = (List<Room>) roomsActual;
+			assertThat(rooms).hasSize(roomsDefinition.size());
 
+			roomsDefinition.forEach((name, squareMeter) -> {
+				assertThat(rooms).anySatisfy(room -> {
+					assertThat(room)
+						.hasFieldOrPropertyWithValue("name", name)
+						.hasFieldOrPropertyWithValue("squareMeter.size", squareMeter);
+				});
+			});
+		});
 	}
 }
