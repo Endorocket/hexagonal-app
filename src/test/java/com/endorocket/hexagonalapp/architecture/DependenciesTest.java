71b4aeb0e729be951dced57026d97d1a786f31eb
Endorocket <43682886+Endorocket@users.noreply.github.com>
2021-06-29 10:26:34 +0200

    Introduce Build - run ArchitectureTest category

--- src/test/java/com/endorocket/hexagonalapp/architecture/DependenciesTest.java
+++ src/test/java/com/endorocket/hexagonalapp/architecture/DependenciesTest.java
@@ -1,8 +1,10 @@
 package com.endorocket.hexagonalapp.architecture;
 
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("ArchitectureTest")
 class DependenciesTest {
   @Test
   void shouldHaveNoCycles() {
