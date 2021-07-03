7df21082b8d8dda6c9e36f7f10cd6fc9ae43cada
Endorocket <endorocket@gmail.com>
2021-07-03 23:59:40 +0200

    HotelRoomOffer - TDD - GREEN - verify if money is higher than zero

--- src/main/java/com/endorocket/hexagonalapp/domain/hotelroomoffer/Money.java
+++ src/main/java/com/endorocket/hexagonalapp/domain/hotelroomoffer/Money.java
@@ -10,7 +10,14 @@ class Money {
     this.value = value;
   }
 
-  static Money of(BigDecimal value) {
-    return new Money(value);
+  static Money of(BigDecimal price) {
+    if (isNotHigherThanZero(price)) {
+      throw new NotAllowedMoneyValueException(price);
+    }
+    return new Money(price);
+  }
+
+  private static boolean isNotHigherThanZero(BigDecimal price) {
+    return price.compareTo(BigDecimal.ZERO) <= 0;
   }
 }
--- src/main/java/com/endorocket/hexagonalapp/domain/hotelroomoffer/NotAllowedMoneyValueException.java
+++ src/main/java/com/endorocket/hexagonalapp/domain/hotelroomoffer/NotAllowedMoneyValueException.java
@@ -1,4 +1,10 @@
 package com.endorocket.hexagonalapp.domain.hotelroomoffer;
 
+import java.math.BigDecimal;
+
 public class NotAllowedMoneyValueException extends RuntimeException {
+
+  public NotAllowedMoneyValueException(BigDecimal price) {
+    super("Given " + price + " is not higher than zero.");
+  }
 }
